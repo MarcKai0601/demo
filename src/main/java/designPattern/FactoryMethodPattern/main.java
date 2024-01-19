1d3d0bd6a95d0b161d4ae9bec35eda749bda2dae
Kai <zheng.kai@jyintgroup.com>
2024-01-19 14:20:04 +0800

    建立工程模式pattern

---
+++ src/main/java/designPattern/FactoryMethodPattern/main.java
@@ -0,0 +1,13 @@
+package designPattern.FactoryMethodPattern;
+
+public class main {
+
+    public static void main(String[] args) {
+        Factory friesFac = new FrenchFriesFactory();
+        Product fries = friesFac.getProduct();
+        Product myfries = ((FrenchFriesFactory) friesFac).getProduct("無鹽的");
+
+        fries.describe();//我是有鹽巴薯條
+        myfries.describe();//我是無鹽的薯條
+    }
+}
